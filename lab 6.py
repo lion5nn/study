41d08d5229d377bcc5bcab1ca36de244bae2f6e4
Borisov Leonid <stiv902931@gmail.com>
2021-02-05 15:30:58 +0300

    It is the first commit of my new version. I realized the game main loop and i created screed and clock

---
+++ lab 6.py
@@ -0,0 +1,33 @@
+import pygame
+from pygame.draw import *
+from random import randint
+pygame.init()
+
+
+FPS = 2
+screen_width = 1400
+screen_height = 900
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+YELLOW = (255, 255, 0)
+GREEN = (0, 255, 0)
+MAGENTA = (255, 0, 255)
+CYAN = (0, 255, 255)
+BLACK = (0, 0, 0)
+COLORS = [RED, BLUE, YELLOW, GREEN, MAGENTA, CYAN]
+
+pygame.display.update()
+clock = pygame.time.Clock()
+finished = False
+
+while not finished:
+    clock.tick(FPS)
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+    pygame.display.update()
+    screen.fill(BLACK)
+
+pygame.quit()
